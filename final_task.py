1bcabf0dedd56d42ca52086cf1d8ce29b1675d3d
ElinorPolity <80193620+ElinorPolity@users.noreply.github.com>
2021-06-01 22:46:15 +0300

    Update final_task.py

--- final_task.py
+++ final_task.py
@@ -11,21 +11,23 @@ else:
     
 soup = BeautifulSoup(response.content, 'html.parser')
 
-#df=ps.DataFrame().columns("prodName","category","price","Image_url","description")
-category="שולחנות קפה"
+tag_b =  soup.findAll("figure", class_ = "product-list-item-thumbnail")
+i = 0
+df=ps.DataFrame(columns=["prodName","price","Image_url"])
 another_row=list()
-tags = soup.find_all(class_="monserrat")
-print(tags)
-for tag in tags:
-    tag=tag.get('href')
-    url_page=url+tag
-    response1=requests.get(url_page)
-    if not response.status_code == 200:
-        results_page = BeautifulSoup(response.content, 'html.parser')
-        prodName=results_page.find("h1", class_="page-title monserrat").get_text()
-        price=results_page.find().get_text()
-        Image_url=results_page.find().get('href')
-        description=results_page.find().get_text()
-        Size=results_page.find().get_text()
-        another_row=[prodName,price,Image_url,description,Size]
-        df.append(another.row)
+for tag in tag_b:
+    children = tag_b[i].find("a")
+    address =  "https://kualastyle.com" +  children.get("href") 
+    response1=requests.get(address)
+    if response.status_code == 200:
+        results_page = BeautifulSoup(response1.content, 'html.parser')
+        prodName=results_page.find_all("h1", class_="page-title")[0].get_text()
+        prodName=prodName[prodName.find("|")+1:]
+        price=results_page.find_all("span", class_="product-price-minimum money")[0].get_text().strip()
+        Image_url=soup.find_all("img",class_="only-image")[i*2].get("src").strip()
+        description=results_page.find_all("span" ,style="text-align: right;")
+        another_row=[prodName,price,Image_url]
+        df_length = len(df)
+        df.loc[df_length] = another_row
+    i = i + 1
+print(df)
